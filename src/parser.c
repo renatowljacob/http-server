3ac88aa1a45a55fb11abd20b8ea3b281f2973f06
renatowljacob <renato.jacob@fatec.sp.gov.br>
2025-08-05 14:40:03 -0300

    WIP: request target parsing

--- src/parser.c
+++ src/parser.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -18,7 +19,7 @@ char *METHODS[] = {
     "TRACE"
 };
 
-// TODO: Come up with better error handling (everything returns NULL)
+// TODO: Come up with better error handling (everything returns -1)
 
 methods parse_method(char *request)
 {
@@ -31,7 +32,6 @@ methods parse_method(char *request)
         end = request;
         method = METHODS[i];
 
-        // NOTE: Think about the overflows!!!
         for (;
             *method != '\0' && *end != ' ';
             end++, method++
@@ -47,121 +47,42 @@ methods parse_method(char *request)
         if (is_equal)
         {
             request = end;
-            return (methods) i;
+            return i;
         }
     }
 
     return -1;
 }
 
-char *parse_request_target(char *request)
+HTTP_versions parse_protocol(char *request)
 {
-    // Absolute URIs must start with a scheme, hence check for 'h' in that
-    // scenario
-    switch (*request)
+    if (!strncmp("HTTP/1.0", request, HTTP_VERSION_SCHEME_SIZE - 1))
     {
-        case '*': return malloc(sizeof("*"));
-        case '/': return parse_request_target_origin_form(request);
-        case 'h': return parse_request_target_absolute_form(request);
-        default: return NULL;
+        return HTTP_1_0;
     }
-}
-
-char *parse_request_target_origin_form(char *request)
-{
-    char *buf;
-    size_t buf_size = 0;
-
-    // Get the end of file path
-    while (*request != ' ')
+    else if (!strncmp("HTTP/1.1", request, HTTP_VERSION_SCHEME_SIZE - 1))
     {
-        if (*request == '\0')
-        {
-            return NULL;
-        }
-        request++;
-        buf_size++;
+        return HTTP_1_1;
+    }
+    else if (!strncmp("HTTP/2.0", request, HTTP_VERSION_SCHEME_SIZE - 1))
+    {
+        return HTTP_2_0;
+    }
+    else if (!strncmp("HTTP/3.0", request, HTTP_VERSION_SCHEME_SIZE - 1))
+    {
+        return HTTP_3_0;
     }
 
-    buf = malloc(buf_size);
-    if (buf == NULL)
-    {
-        return NULL;
-    }
-    memcpy(buf, request - buf_size, buf_size);
-    buf[buf_size] = '\0';
+    request += HTTP_VERSION_SCHEME_SIZE - 1;
 
-    return buf;
-}
-
-// TODO: Get host from request target and ignore Host header field
-char *parse_request_target_absolute_form(char *request)
-{
-    char *buf;
-    char *host_start, *host_end;
-    char *scheme;
-    size_t scheme_size = 0;
-    size_t buf_size = 0;
-
-    // NOTE: Consider doing all this in one loop (though it may get convoluted)
-
-    // Get scheme and determine start of host information in request target
-    if (strncmp(request, HTTP_SCHEME, sizeof(HTTP_SCHEME) - 1))
-    {
-        scheme = HTTP_SCHEME;
-    }
-    else if (strncmp(request, HTTPS_SCHEME, sizeof(HTTPS_SCHEME) - 1))
-    {
-        scheme = HTTPS_SCHEME;
-    }
-    else
-    {
-        return NULL;
-    }
-    scheme_size = sizeof(scheme) - 1;
-    request += scheme_size;
-    buf_size += scheme_size;
-    host_start = request;
-
-    // Get end of host information
-    while (*request != '/' && *request != ' ')
-    {
-        if (*request == '\0')
-        {
-            return NULL;
-        }
-
-        request++;
-        buf_size++;
-    }
-    host_end = request - 1;
-
-    // Parse rest of URI
-    while (*request != ' ')
-    {
-        if (*request == '\0')
-        {
-            return NULL;
-        }
-
-        request++;
-        buf_size++;
-    }
-
-    buf = malloc(buf_size);
-    if (buf == NULL)
-    {
-        return NULL;
-    }
-    memcpy(buf, request - buf_size, buf_size);
-    buf[buf_size] = '\0';
-
-    return buf;
+    return -1;
 }
 
 char *parse_request(char *request)
 {
-    char *buf;
+    char *request_target_buf;
+    size_t request_target_buf_size = 0;
+    HTTP_versions version;
     methods method = parse_method(request);
 
     // We don't support CONNECT
@@ -177,11 +98,121 @@ char *parse_request(char *request)
     }
     request++;
 
-    buf = parse_request_target(request);
-    if (buf == NULL)
+    if (parse_request_target(
+        request,
+        request_target_buf,
+        &request_target_buf_size
+    ) == -1)
     {
         return NULL;
     }
+    request++;
+    assert(request_target_buf != NULL);
 
-    return buf;
+    version = parse_protocol(request);
+    switch (version)
+    {
+        case HTTP_1_0: break; // Downgrade
+        default: break; // Proceed normally
+    }
+    request++;
+
+    return request_target_buf;
+}
+
+int32_t parse_request_target(char *request, char *buf, size_t *buf_size)
+{
+    // Absolute URIs must start with a scheme, thus starting character must be
+    // a 'h'
+    switch (*request)
+    {
+        case '*':
+            *buf_size = sizeof("*");
+            buf = malloc(sizeof(buf_size));
+            return 0;
+        case '/':
+            return parse_request_target_origin_form(request, buf, buf_size);
+        case 'h':
+            return parse_request_target_absolute_form(request, buf, buf_size);
+        default:
+            return -1;
+    }
+}
+
+// TODO: Get host from request target and ignore Host header field
+int32_t parse_request_target_absolute_form(char *request, char *buf, size_t *buf_size)
+{
+    char *host_start, *host_end;
+    char *scheme;
+    size_t _buf_size = *buf_size;
+    size_t scheme_size = 0;
+
+    // Get scheme and determine the start of host information in request target
+    if (!strncmp(request, HTTP_SCHEME, sizeof(HTTP_SCHEME) - 1))
+    {
+        scheme = HTTP_SCHEME;
+    }
+    else if (!strncmp(request, HTTPS_SCHEME, sizeof(HTTPS_SCHEME) - 1))
+    {
+        scheme = HTTPS_SCHEME;
+    }
+    else
+    {
+        return NULL;
+    }
+    scheme_size = sizeof(scheme) - 1;
+    request += scheme_size;
+    _buf_size += scheme_size;
+    host_start = request;
+
+    // Get end of host information
+    while (*request != '/' && *request != ' ')
+    {
+        if (*request == '\0')
+        {
+            return -1;
+        }
+
+        request++;
+        _buf_size++;
+    }
+    host_end = request - 1;
+
+    // Since what's left of the URI is only the path and queries portions, we
+    // can parse the rest as origin-form
+    *buf_size = _buf_size;
+    if (parse_request_target_origin_form(request, buf, buf_size) == -1)
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+// TODO: deduplicate this code (pass a buf as a pointer)
+int32_t parse_request_target_origin_form(char *request, char *buf, size_t *buf_size)
+{
+    size_t _buf_size = *buf_size;
+
+    // Get the end of filepath
+    while (*request != ' ')
+    {
+        if (*request == '\0')
+        {
+            return -1;
+        }
+        request++;
+        buf_size++;
+    }
+
+    buf = malloc(_buf_size);
+    if (buf == NULL)
+    {
+        return -1;
+    }
+    memcpy(buf, request - _buf_size, _buf_size);
+    buf[_buf_size] = '\0';
+    *buf_size = _buf_size;
+
+    return 0;
 }
--- src/parser.h
+++ src/parser.h
@@ -1,10 +1,15 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stddef.h>
+#include <stdint.h>
 #define DEFAULT_FILE "index.html"
 
 #define HTTP_SCHEME "http://"
 #define HTTPS_SCHEME "https://"
+
+#define HTTP_VERSION_SCHEME "HTTP/X.X"
+#define HTTP_VERSION_SCHEME_SIZE sizeof(HTTP_VERSION_SCHEME)
 
 #define MAX_METHOD_LENGTH 7
 #define METHODS_NUMBER 8
@@ -21,6 +26,23 @@ typedef enum {
     TRACE
 } methods;
 
+// HTTP versions
+typedef enum {
+    HTTP_1_0,
+    HTTP_1_1,
+    HTTP_2_0,
+    HTTP_3_0,
+} HTTP_versions;
+
+/**
+ * @brief Get method from request.
+ * @param request Pointer to request initial character. If a valid method is
+ * found, it will be mutated to point to the last method character.
+ */
+methods parse_method(char *request);
+
+HTTP_versions parse_protocol(char *request);
+
 /**
  * @brief Parse request-target from request message
  * @param request Pointer to request
@@ -30,17 +52,10 @@ typedef enum {
  */
 char *parse_request(char *request);
 
-char *parse_request_target(char *request);
+int32_t parse_request_target(char *request, char *buf, size_t *buf_size);
 
-char *parse_request_target_origin_form(char *request);
+int32_t parse_request_target_absolute_form(char *request, char *buf, size_t *buf_size);
 
-char *parse_request_target_absolute_form(char *request);
-
-/**
- * @brief Get method from request.
- * @param request Pointer to request initial character. If a valid method is
- * found, it will be mutated to point to the last method character.
- */
-methods parse_method(char *request);
+int32_t parse_request_target_origin_form(char *request, char *buf, size_t *buf_size);
 
 #endif
